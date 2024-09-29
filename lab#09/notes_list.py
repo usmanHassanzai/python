478b62e5c0813372da13ab0be3e9c15c411a9d5a
Usman Muzafar Shah <usmankhan14700@gmail.com>
2024-09-29 19:21:49 +0500

    here are the function of list

---
+++ lab#09/notes_list.py
@@ -0,0 +1,13 @@
+list = [1,2,3,4,5]
+list.append(6)
+list.insert(-1 , 10)
+list.remove(10)
+print(list.index(2))
+list.reverse()
+print(list)
+list.sort()
+print(len(list))
+list.count(5)
+lst = [1,2,3,1,5,1,6,1,8,3,2,5]
+print(set(lst))
+print(list)
