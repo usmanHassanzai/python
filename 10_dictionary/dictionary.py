6d1c0e5dbb55700572a7258b1a41582f705d54c5
Usman Muzafar Shah <usmankhan14700@gmail.com>
2024-09-29 19:51:55 +0500

    add dictionary concept

---
+++ 10_dictionary/dictionary.py
@@ -0,0 +1,18 @@
+Introduction = {
+    
+    "Name"          :   "My name is usman.",
+    "Father_Name"   :   "My father name is Muzafar Shah.",
+    "University"    :   "FAST National University Of Computer And Emerging Sciences.",
+    "Program"       :   "Computer Science",
+    "Batch"         :   22
+    
+    
+}
+
+print(Introduction)
+print(Introduction['Name'])
+print(Introduction["Father_Name"])
+print(Introduction["Batch"])
+# Introduction.items()
+# print(Introduction)s
+print(Introduction.items()) #converting dictionary into tuples
